9b753a36e8d76f688055f232e3f943c286cd745b
Brian Lee <the930325@yahoo.com>
2021-12-05 17:42:53 -0500

    new base map and preset view window

--- ap/apuv_map/app.R
+++ ap/apuv_map/app.R
@@ -16,14 +16,11 @@ library(tigris)
 setwd("/Users/beelee/Desktop/Columbia/Fall_2021/P8105-Data_Science/p8105-final_project/ap/apuv_map/")
 apuv_df = readRDS("apuv.RDS")
 ext_val = readRDS("ext_val.RDS")
-seasoned_apuv_df = readRDS("apuv.RDS")
-seasoned_ext_val = readRDS("ext_val.RDS")
+us_counties = tigris::counties(c("NY", "PA", "OH"))
 
-us_counties = 
-  tigris::counties(c("NY", "PA", "OH"))
-
+# Filter df for the year desired and merge with the sf object
 filter_merge = function(y){
-  seasoned_apuv_df %>% 
+  apuv_df %>% 
     filter(year == y) %>%
     pivot_wider(
       names_from = season,
@@ -32,6 +29,54 @@ filter_merge = function(y){
     ) %>% 
     geo_join(us_counties, ., "GEOID",  "countyfips") %>%
     sf::st_transform('+proj=longlat +datum=WGS84')
+}
+
+# Return the filtered and merged object
+select_year_season = function(y, s){
+  
+  if(y == 2001){res = merged_2001_df}
+  if(y == 2002){res = merged_2002_df}
+  if(y == 2003){res = merged_2003_df}
+  if(y == 2004){res = merged_2004_df}
+  if(y == 2005){res = merged_2005_df}
+  if(y == 2006){res = merged_2006_df}
+  if(y == 2007){res = merged_2007_df}
+  if(y == 2008){res = merged_2008_df}
+  if(y == 2009){res = merged_2009_df}
+  if(y == 2010){res = merged_2010_df}
+  if(y == 2011){res = merged_2011_df}
+  if(y == 2012){res = merged_2012_df}
+  if(y == 2013){res = merged_2013_df}
+  if(y == 2014){res = merged_2014_df}
+  if(y == 2015){res = merged_2015_df}
+  if(y == 2016){res = merged_2016_df}
+  
+  if(s == "Spring"){
+    res = 
+      res %>% 
+      select(-contains(c("Summer", "Fall", "Winter"))) %>% 
+      rename_all(funs(str_replace_all(., "Spring_", "")))
+  }
+  if(s == "Summer"){
+    res =
+      res %>% 
+      select(-contains(c("Fall", "Winter", "Spring"))) %>% 
+      rename_all(funs(str_replace_all(., "Summer_", "")))
+  }
+  if(s == "Fall"){
+    res =
+      res %>% 
+      select(-contains(c("Winter", "Spring", "Summer"))) %>% 
+      rename_all(funs(str_replace_all(., "Fall_", "")))
+  }
+  if(s == "Winter"){
+    res =
+      res %>% 
+      select(-contains(c("Spring", "Summer", "Fall"))) %>% 
+      rename_all(funs(str_replace_all(., "Winter_", "")))
+  }
+  
+  return(res)
 }
 
 merged_2001_df = filter_merge(2001)
@@ -51,61 +96,6 @@ merged_2014_df = filter_merge(2014)
 merged_2015_df = filter_merge(2015)
 merged_2016_df = filter_merge(2016)
 
-select_year = function(y){
-  
-  if(y == 2001){res = merged_2001_df}
-  if(y == 2002){res = merged_2002_df}
-  if(y == 2003){res = merged_2003_df}
-  if(y == 2004){res = merged_2004_df}
-  if(y == 2005){res = merged_2005_df}
-  if(y == 2006){res = merged_2006_df}
-  if(y == 2007){res = merged_2007_df}
-  if(y == 2008){res = merged_2008_df}
-  if(y == 2009){res = merged_2009_df}
-  if(y == 2010){res = merged_2010_df}
-  if(y == 2011){res = merged_2011_df}
-  if(y == 2012){res = merged_2012_df}
-  if(y == 2013){res = merged_2013_df}
-  if(y == 2014){res = merged_2014_df}
-  if(y == 2015){res = merged_2015_df}
-  if(y == 2016){res = merged_2016_df}
-  
-  return(res)
-}
-
-select_season = function(df, s){
-  if(s == "Spring"){
-    res = 
-      df %>% 
-      select(-contains(c("Summer", "Fall", "Winter"))) %>% 
-      rename_all(funs(str_replace_all(., "Spring_", "")))
-  }
-  if(s == "Summer"){
-    res =
-      df %>% 
-      select(-contains(c("Fall", "Winter", "Spring"))) %>% 
-      rename_all(funs(str_replace_all(., "Summer_", "")))
-  }
-  if(s == "Fall"){
-    res =
-      df %>% 
-      select(-contains(c("Winter", "Spring", "Summer"))) %>% 
-      rename_all(funs(str_replace_all(., "Fall_", "")))
-  }
-  if(s == "Winter"){
-    res =
-      df %>% 
-      select(-contains(c("Spring", "Summer", "Fall"))) %>% 
-      rename_all(funs(str_replace_all(., "Winter_", "")))
-  }
-  
-  return(res)
-}
-
-test_df = select_year(2001)
-test_df= select_season(test_df, "Spring")
-test_df
-
 ui = fluidPage(
   
   titlePanel("Annual Air Pollution and UV Radiation Map"),
@@ -113,7 +103,7 @@ ui = fluidPage(
   sidebarLayout(
     sidebarPanel(
       tags$a(href = "https://ephtracking.cdc.gov/download", "Data Source", target = "_blank"),
-      h5("Climate and a few particular chronic disease risks are known to be highly correlated. Let's take a look at the Particulate Matter //expression(PM_{2.5}), Ozone (O_{3}) and UV radiation (edd) levels over the years in New York, Pennsylvania, and Ohio. "),
+      h5("Some climate conditions and a few particular chronic disease risks are known to be highly correlated. Let's explore the Particulate Matter //expression(PM_{2.5}), Ozone (O_{3}) and UV radiation (edd) levels over the years in New York, Pennsylvania, and Ohio."),
       selectInput("yr", "Select a year", choices = unique(apuv_df$year)),
       selectInput("ss", "Select a season", choices = unique(apuv_df$season))
     ),
@@ -132,8 +122,7 @@ server = function(input, output) {
   
   pm_oz_uv = reactive({
     
-    pou = select_year(input$yr)
-    pou = select_season(pou, input$ss)
+    pou = select_year_season(input$yr, input$ss)
     return(pou)
     
   })
@@ -143,8 +132,8 @@ server = function(input, output) {
     # labels = sprintf("some label")
     pm_oz_uv() %>% 
       leaflet() %>% 
-      addProviderTiles(provider = "CartoDB.Positron") %>% 
-      # addProviderTiles(provider = "Stamen.TonerBackground") %>% 
+      addProviderTiles(provider = "Stamen.Toner") %>% 
+      setView(-78, 41.8, zoom = 5.5) %>% 
       addPolygons(stroke = FALSE,
                   smoothFactor = 0.5,
                   opacity = 1,
@@ -166,7 +155,8 @@ server = function(input, output) {
     pal = colorBin(palette = "OrRd", 9, domain = c(as.numeric(ext_val[5]),as.numeric(ext_val[2])))
     pm_oz_uv() %>% 
       leaflet() %>% 
-      addProviderTiles(provider = "CartoDB.Positron") %>% 
+      addProviderTiles(provider = "Stamen.Toner") %>% 
+      setView(-78, 41.8, zoom = 5.5) %>% 
       addPolygons(stroke = FALSE,
                   smoothFactor = 0.5,
                   opacity = 1,
@@ -188,7 +178,8 @@ server = function(input, output) {
     pal = colorBin(palette = "PuBu", 9, domain = c(as.numeric(ext_val[6]),as.numeric(ext_val[3])))
     pm_oz_uv() %>% 
       leaflet() %>% 
-      addProviderTiles(provider = "CartoDB.Positron") %>% 
+      addProviderTiles(provider = "Stamen.Toner") %>% 
+      setView(-78, 41.8, zoom = 5.5) %>% 
       addPolygons(stroke = FALSE,
                   smoothFactor = 0.5,
                   opacity = 1,
